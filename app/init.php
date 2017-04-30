828df150656cdccf56dacc7bd74d2cfb6b1856da
Kevin Nderitu <nderitukelvin19@gmail.com>
2017-04-30 16:02:09 +0300

    Added dependancies

--- app/init.php
+++ app/init.php
@@ -1,10 +1,10 @@
 <?php 
-
+require_once 'dirs.php';
+require_once 'env.php';
 function __autoload($class){
     $class = strtolower($class);
     $path = __DIR__ . DIRECTORY_SEPARATOR . "core" . DIRECTORY_SEPARATOR . "{$class}.php";
     if(file_exists($path)){
-    	echo "Found";
         require_once($path);
     }else{
         die("The file {$class}.php couldn't be found in ($path}");
